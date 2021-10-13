3630e21dbd8f5b24d52b5cc280dfffd8a3966299
Slk <212234466@qq.com>
2021-10-13 08:40:50 +0800

    first commit

---
+++ src/main/java/com/atguigu/GitTest.java
@@ -0,0 +1,11 @@
+package com.atguigu;
+
+/**
+ * @author shkstart
+ * @create 2021-10-13 8:31
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello,git!");
+    }
+}
